30b531f17f24523f3ff6284779a42c5f03d242d7
William Chen <39677488+wanchichen@users.noreply.github.com>
2025-01-21 17:39:55 -0500

    Update expert.py

--- s3prl/upstream/espnet_hubert/expert.py
+++ s3prl/upstream/espnet_hubert/expert.py
@@ -10,25 +10,38 @@ logger = logging.getLogger(__name__)
 
 try:
     from espnet2.tasks.hubert import HubertTask
+    from espnet2.tasks.ssl import SSLTask
 except ModuleNotFoundError:
     HubertTask = None
     logger.warning("ESPnet is not installed, cannot use espnet_hubert upstream")
 
 
 class UpstreamExpert(torch.nn.Module):
-    def __init__(self, ckpt, config=None, **kwargs):
+    def __init__(self, ckpt, config=None, use_torchaudio=False, **kwargs):
         super().__init__()
         device = "cuda" if torch.cuda.is_available() else "cpu"
         assert (
             HubertTask is not None
         ), "ESPnet is not installed, run `external_tools/install_espnet.sh` to install"
-        hubert_model, hubert_train_args = HubertTask.build_model_from_file(
-            config,
-            ckpt,
-            device,
-        )
+        
+        self.use_torchaudio = use_torchaudio
+
+        if use_torchaudio:
+            hubert_model, hubert_train_args = HubertTask.build_model_from_file(
+                config,
+                ckpt,
+                device,
+            )
+            self.model = hubert_model.encoder.hubert_pretrain_model
+        else:
+            hubert_model, hubert_train_args = SSLTask.build_model_from_file(
+                config,
+                ckpt,
+                device,
+            )
+            self.model = hubert_model
+
         self.device = next(hubert_model.parameters()).device
-        self.model = hubert_model.encoder.hubert_pretrain_model
 
     def get_downsample_rates(self, key: str = None) -> int:
         return 320
@@ -36,8 +49,11 @@ class UpstreamExpert(torch.nn.Module):
     def forward(self, wavs):
         wav_lengths = torch.LongTensor([len(wav) for wav in wavs]).to(self.device)
         wavs = pad_sequence(wavs, batch_first=True).to(self.device)
-        feats = self.model.wav2vec2.extract_features(wavs, wav_lengths)[
-            0
-        ]  # (time, feat_dim))
+        if self.use_torchaudio:
+            feats = self.model.wav2vec2.extract_features(wavs, wav_lengths)[
+                0
+            ]  # (time, feat_dim))
+        else:
+            feats, mask_info, pen = self.model.encode(wavs, wav_lengths, use_mask=self.training, use_final_output=False)
 
         return {"hidden_states": feats}
